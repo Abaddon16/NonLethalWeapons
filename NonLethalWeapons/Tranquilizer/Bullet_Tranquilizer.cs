047ee2919ebf7f2db0f3cb876271d6e062a943e4
Logan Gombar <logan.gombar@gmail.com>
2018-12-15 21:17:46 -0600

    made it so body size has an impact on tranquilize difficulty. I.e- bigger animals need more darts (thrumbos =(7-12, more towards 12), humans=(1-3, more towards 2)). Added <version> tag to About.xml in the event it matters.

--- NonLethalWeapons/Tranquilizer/Bullet_Tranquilizer.cs
+++ NonLethalWeapons/Tranquilizer/Bullet_Tranquilizer.cs
@@ -1,57 +1,54 @@
 ﻿using RimWorld;
-using Verse;
+using System;
 using System.Collections.Generic;
+using Verse;
 
 
 namespace NonLethalWeapons {
-    /**
-     <summary>  A thing definition class of a non-lethal bullet. </summary>
-     <remarks>  Abaddon16, 12/15/2018. </remarks>
-     <seealso cref="T:Verse.ThingDef"/>
-     */
+	/**
+		<summary>  A thing definition class of a non-lethal bullet. </summary>
+		<remarks>  Abaddon16, 12/15/2018. </remarks>
+		<seealso cref="T:Verse.ThingDef"/>
+	*/
 	public class ThingDef_NonLethalBullet:ThingDef {
-
 		/** <summary>  The chance of adding the hediffs. </summary> */
 		public float AddHediffChance;
 		/** <summary>  A list of the hediffs to add. </summary> */
 		public List<HediffDef> HediffsToAdd;
 	}
 
-    /**
-     <summary>  A tranquilizer bullet object. </summary>
-     <remarks> Abaddon16, 12/15/2018. </remarks>
-     <seealso cref="T:RimWorld.Bullet"/>
-     */
+	/**
+		<summary>  A tranquilizer bullet object. </summary>
+		<remarks> Abaddon16, 12/15/2018. </remarks>
+		<seealso cref="T:RimWorld.Bullet"/>
+	*/
 	public class Projectile_TranquilizerBullet:Bullet {
 		#region Properties
-
-        /**
-         <summary>  Gets the definition of the non-lethal bullet </summary>
-         <value>    The definition class. </value>
-         */
+		/**
+			<summary>  Gets the definition of the non-lethal bullet </summary>
+			<value> The definition class. </value>
+		*/
 		public ThingDef_NonLethalBullet Def => this.def as ThingDef_NonLethalBullet;
 		#endregion Properties
 
 		#region Overrides
-
-        /**
-         <summary>  Code called on impact </summary>
-         <remarks>  Abaddon16, 12/15/2018. </remarks>
-         <param name="hitThing"> The thing being hit. </param>
-         */
+		/**
+			<summary>  Code called on impact </summary>
+			<remarks>  Abaddon16, 12/15/2018. </remarks>
+			<param name="hitThing"> The thing being hit. </param>
+		*/
 		protected override void Impact(Thing hitThing) {
 			base.Impact(hitThing);
-
-			
-			if(Def!=null && hitThing!=null && hitThing is Pawn hitPawn){
+			if(this.Def!=null&&hitThing!=null&&hitThing is Pawn hitPawn) {
 				if(!hitPawn.RaceProps.IsFlesh)
 					return;
-                foreach(HediffDef HediffToAdd in Def.HediffsToAdd) {
-					var rand = Rand.Value;
-					if(rand<=Def.AddHediffChance) {
-						var hediffOnPawn = hitPawn?.health?.hediffSet?.GetFirstHediffOfDef(HediffToAdd);
-						float randomSeverity = Rand.Range(0.1f, 0.3f);
-						if(hediffOnPawn!=null) hediffOnPawn.Severity+=randomSeverity;
+				foreach(HediffDef HediffToAdd in this.Def.HediffsToAdd) {
+					float rand = Rand.Value;
+					if(rand<=this.Def.AddHediffChance) {
+						Hediff hediffOnPawn = hitPawn?.health?.hediffSet?.GetFirstHediffOfDef(HediffToAdd);
+						float randomSeverity = Rand.Range(0.25f, 0.4285f)/(float)Math.Pow(hitPawn.RaceProps.baseBodySize, 1.5f);
+						if(hediffOnPawn!=null)
+							hediffOnPawn.Severity+=randomSeverity;
 						else {
 							Hediff hediff = HediffMaker.MakeHediff(HediffToAdd, hitPawn, null);
 							hediff.Severity=randomSeverity;
